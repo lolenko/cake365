6967b0a18604db4164f2c2d4bf378e1e41a00fb0
lolenko <lolenko@finch-melrose.com>
2014-12-12 00:03:26 +0300

    Поигрался с Jade, перевёл дефолтные сайловские вьюхи на него

---
+++ api/controllers/admin/ModelController.js
@@ -0,0 +1,11 @@
+/**
+ * Admin/modelController
+ *
+ * @description :: Server-side logic for managing admin/models
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
